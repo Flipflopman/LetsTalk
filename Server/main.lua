90ee65d0f41d0b23b7050cf7f1c6ff380da24b42
Flipflopman <66565504+Flipflopman@users.noreply.github.com>
2023-08-14 23:49:06 -0400

    Create main.lua

---
+++ Server/main.lua
@@ -0,0 +1,6 @@
+-- This is the primary server.
+-- A combination of MySql + LuaNode should get this up and running.
+-- Its gonna be great to write ts in lua instead of node.js. (I do not like node.js)
+-- Side note: Since I'm using LuaNode and a sql lua framework, will they both work together? 
+-- SQL Framework: https://lunarmodules.github.io/luasql/
+-- Framework: https://github.com/ignacio/LuaNode (hopefully)
